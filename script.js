04b07dd44c4a23dbc9139288b9c20b8ebcd8ebad
benaor <benjamingirard25@gmail.com>
2020-02-13 10:43:07 +0100

    fin mobile version and menu burger

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function openBurger() {
+    document.querySelector(".navTel").style.display = "flex";
+}
+function closeBurger() {
+    document.querySelector(".navTel").style.display = "none";
+}
