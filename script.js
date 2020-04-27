284e2e1ef6dcb1589b008b194b5f2f85a57a776b
benaor <benjamingirard25@gmail.com>
2020-04-27 19:09:38 +0200

    create button Dark/clea

--- script.js
+++ script.js
@@ -16,13 +16,28 @@ function modeSombre() {
 
     if (darkTheme == false) {
 
-        alert('mode sombre activé');
+        //Active darky mode 
+        document.querySelector(':root').style.setProperty("--var-color-bg-nav", '#e8b5db');//ok
+        document.querySelector(':root').style.setProperty("--var-color-border-section", '#333333');//ok
+        document.querySelector(':root').style.setProperty("--var-color-contour-profil", '#d476bc');
+        document.querySelector(':root').style.setProperty("--var-color-font-standard", '#cecece');
+        document.querySelector(':root').style.setProperty("--var-color-text-contenu", '#c0c0c0');
+        document.querySelector(':root').style.setProperty("--var-color-white", '#121212');//ok
+
+        document.getElementById('logo-fb-header').innerHTML = '<img src="images/iconFb-ms.png" alt="logo-fb" class="logo-fb">'
         btnDark.innerHTML ='<i class="fas fa-moon"></i>';
         return darkTheme = true;
 
     } else {
 
-        alert('mode claire activé');
+        //active clear mode
+        document.querySelector(':root').style.setProperty("--var-color-bg-nav", '#5d23a0');
+        document.querySelector(':root').style.setProperty("--var-color-border-section", '#cecece');
+        document.querySelector(':root').style.setProperty("--var-color-contour-profil", '#8228b6');
+        document.querySelector(':root').style.setProperty("--var-color-font-standard", '#343a40');
+        document.querySelector(':root').style.setProperty("--var-color-text-contenu", '#868e96');
+        document.querySelector(':root').style.setProperty("--var-color-white", '#ffffff');
+
         btnDark.innerHTML ='<i class="fas fa-sun"></i>';
         return darkTheme = false;
 
